5e3fb15130d54d47cac65e8883843d31ed8f3b9a
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-10-27 13:55:05 +0200

    dynamically generate image for herbie, depending on death stage

--- src/herbie.js
+++ src/herbie.js
@@ -6,8 +6,11 @@ import herbieDying3 from './herbieDying3.png';
 import herbieDying4 from './herbieDying4.png';
 
 class Herbie extends React.Component {
-    
     render() {
+        var herbieImageString = require("./herbie.png");
+        if(this.props.dying !== 0 && this.props.dying !== 5){
+            herbieImageString = require("./herbieDying" + this.props.dying + ".png");
+        }
         var herbieStyle = {
             height: this.props.size,
             width: this.props.size,
@@ -17,7 +20,7 @@ class Herbie extends React.Component {
             transform: "translate(-50%, -50%)"
         };
         return (
-            <img src={herbie} style={herbieStyle} alt="Herbie" />
+            <img src={herbieImageString} style={herbieStyle} alt="Herbie" />
         );
     }
 };
