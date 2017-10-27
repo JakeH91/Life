872fda3d5c75f1fbf27dd755deb1aeb4ce1a2ab6
Jake Hill <Jake_hill666@hotmail.co.uk>
2017-10-27 13:13:40 +0200

    import herbie Dying images into Herbie component

--- src/herbie.js
+++ src/herbie.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import herbie from './herbie.png';
+import herbieDying1 from './herbieDying1';
+import herbieDying2 from './herbieDying2';
+import herbieDying3 from './herbieDying3';
+import herbieDying4 from './herbieDying4';
 
 class Herbie extends React.Component {
+    
     render() {
         var herbieStyle = {
             height: this.props.size,
